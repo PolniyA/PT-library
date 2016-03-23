f8fd3784ee99553f89c85a92388c1fbdfec0bd3c
Ефимов Сергей Александрович <sefimov@at-consulting.ru>
2016-03-23 18:40:54 +0300

    Add bookController create and display

--- api/controllers/BookController.js
+++ api/controllers/BookController.js
@@ -6,6 +6,28 @@
  */
 
 module.exports = {
-	
+
+  'new': function (req, res) {
+    res.view();
+  },
+
+  'create': function (req, res, next) {
+    Book.create(req.params.all(), function bookCreated(err, book) {
+      if (err) return next(err);
+
+      res.redirect('/book/show/' + book.id)
+    });
+  },
+
+  'show': function (req, res, next) {
+    Book.findOne(req.param('id'), function foundBook(err, book) {
+      if (err) return next(err);
+      if (!book) return next();
+
+      res.view({
+        book: book
+      })
+    })
+  }
 };
 
